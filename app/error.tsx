88cf0b7e8965e9bc4d23716d522e00e5848cd81f
hubbaka <38581360+hubbaka@users.noreply.github.com>
2023-02-07 19:05:30 +0700

    chore: setup husky

---
+++ app/error.tsx
@@ -0,0 +1,20 @@
+"use client";
+import React, { useEffect } from "react";
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error;
+  reset: () => void;
+}) {
+  useEffect(() => {
+    // Log the error to an error reporting service
+    console.error(error);
+  }, [error]);
+  return (
+    <div>
+      <p>Disini ada error</p> <button onClick={() => reset()}>Refresh</button>
+    </div>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,10 @@
-import './globals.css'
-
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import 'styles/global.css';
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
@@ -12,7 +13,8 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
+
       <body>{children}</body>
     </html>
-  )
+  );
 }
