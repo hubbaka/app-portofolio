b37a2199d67b73ed967bd29869484cecbed076a3
hubbaka <38581360+hubbaka@users.noreply.github.com>
2023-02-09 16:02:33 +0700

    Repair Component

--- components/ProjectSection.tsx
+++ components/ProjectSection.tsx
@@ -30,7 +30,7 @@ const ProjectSection = () => {
         <hr className="w-6 h-1 mx-auto my-4 bg-teal-500 border-0 rounded" />
       </h1>
 
-      <div className="flex flex-col space-y-28">
+      <div className="flex flex-col space-y-10">
         {projects.map((project, idx) => {
           return (
             <div key={idx}>
